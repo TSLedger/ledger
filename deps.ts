03e12a5ec8cae8b8c328368c1ee06dc03f5af39b
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-10-08 15:36:18 -0400

    chore(deps): update to strict versions

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 //   "imports": { "@cm-iv/queue": "jsr:@cm-iv/queue@^1.0.0", "@denostack/superserial": "jsr:@denostack/superserial@^0.3.5" }
 
-export { Queue } from 'jsr:@cm-iv/queue';
+export { Queue } from 'jsr:@cm-iv/queue@1.0.0';
