fa1d44127e7c46dc9df7ec5ed634afcb10d7ac92
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2024-11-18 19:01:46 +0000

    refactor: export level as full type instead of partial type

--- mod.ts
+++ mod.ts
@@ -1,8 +1,8 @@
 import { Ledger, type LedgerOptions } from './index.mod.ts';
 import type { PageMessageContext } from './lib/interface/context.if.ts';
-import type { Level } from './lib/interface/level.if.ts';
+import { Level } from './lib/interface/level.if.ts';
 import type { PageOptions } from './lib/interface/page.if.ts';
 import { Page } from './lib/page.ts';
 
-export { Ledger, Page };
-export type { LedgerOptions, Level, PageMessageContext, PageOptions };
+export { Ledger, Level, Page };
+export type { LedgerOptions, PageMessageContext, PageOptions };
