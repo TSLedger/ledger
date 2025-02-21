2ff84f4b9109aca8904521ceca26e3e0e854a518
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2025-02-21 00:35:25 +0000

    fix: export ServiceBinderOption for handlers

--- lib/struct/export.ts
+++ lib/struct/export.ts
@@ -1,7 +1,7 @@
 import type { DispatchMessageContext } from './interface/context.ts';
 import type { WorkerHandler } from './interface/handler.ts';
 import { Level } from './interface/level.ts';
-import type { BinderOption, LedgerOption } from './interface/options.ts';
+import type { BinderOption, LedgerOption, ServiceBinderOption } from './interface/options.ts';
 
 export { Level };
-export type { BinderOption, DispatchMessageContext, LedgerOption, WorkerHandler };
+export type { BinderOption, DispatchMessageContext, LedgerOption, ServiceBinderOption, WorkerHandler };
