bdf1b4b402f7ed6a210b94d2ac099f5ada70a971
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2025-12-11 18:10:09 +0000

    feat: restructure testing and builds

---
+++ mod.test.ts
@@ -0,0 +1,42 @@
+import { Ledger } from '@ledger/ledger';
+
+let ledger: Ledger;
+
+Deno.test.beforeAll(async () => {
+  ledger = new Ledger({
+    service: 'Test IPC Service',
+    troubleshooting: true,
+    useAsyncDispatchQueue: true,
+  });
+  ledger.register({
+    definition: 'jsr:@ledger/console-handler@0.0.4',
+  });
+  await ledger.alive();
+});
+
+Deno.test('Runtime Definition Test', async (kit) => {
+  await kit.step('poll alive', async () => {
+    await ledger.alive();
+  });
+  await kit.step('validate api', async () => {
+    const object = {
+      name: 'some test',
+      value: 42,
+      error: new Error('Test Error'),
+      array: [1, 2, 3],
+      set: new Set([1, 2, 3]),
+      map: new Map([['key1', 'value1'], ['key2', 'value2']]),
+      nested: {
+        a: 1,
+        b: [true, false, null],
+        c: { d: 'deep' },
+      },
+    };
+
+    ledger.trace('Validating API... (Trace)', object);
+    ledger.information('Validating API... (Information)', object);
+    ledger.warning('Validating API... (Warning)', object);
+    ledger.severe('Validating API... (Severe)', object);
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  });
+});
