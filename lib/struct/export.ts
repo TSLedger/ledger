2dde982241b1766bd77873958f8aedecd8aa0fc0
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2025-12-15 18:43:16 +0000

    feat: update exports to error controls

--- lib/struct/export.ts
+++ lib/struct/export.ts
@@ -1,7 +1,7 @@
-import type { DispatchMessageContext } from './interface/context.ts';
+import type { DispatchMessageContext, LedgerErrorMessageContext, Operation } from './interface/context.ts';
 import type { WorkerHandler } from './interface/handler.ts';
 import { Level } from './interface/level.ts';
 import type { LedgerOption, ServiceHandlerOption } from './interface/options.ts';
 
 export { Level };
-export type { DispatchMessageContext, LedgerOption, ServiceHandlerOption, WorkerHandler };
+export type { DispatchMessageContext, LedgerErrorMessageContext, LedgerOption, Operation, ServiceHandlerOption, WorkerHandler };
