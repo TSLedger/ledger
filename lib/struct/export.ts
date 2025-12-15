8e25d66934ad10f837dd3a6c37e65b6ec771c092
Samuel Voeller <12182390+xCykrix@users.noreply.github.com>
2025-12-15 18:49:11 +0000

    fix: correct operation to be full export & tick ver

--- lib/struct/export.ts
+++ lib/struct/export.ts
@@ -1,7 +1,7 @@
-import type { DispatchMessageContext, LedgerErrorMessageContext, Operation } from './interface/context.ts';
+import { type DispatchMessageContext, type LedgerErrorMessageContext, Operation } from './interface/context.ts';
 import type { WorkerHandler } from './interface/handler.ts';
 import { Level } from './interface/level.ts';
 import type { LedgerOption, ServiceHandlerOption } from './interface/options.ts';
 
-export { Level };
-export type { DispatchMessageContext, LedgerErrorMessageContext, LedgerOption, Operation, ServiceHandlerOption, WorkerHandler };
+export { Level, Operation };
+export type { DispatchMessageContext, LedgerErrorMessageContext, LedgerOption, ServiceHandlerOption, WorkerHandler };
